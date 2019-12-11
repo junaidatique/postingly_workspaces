4ec0f35998d633e66c5ba9cf637a7c3450403fd8
Junaid Atique <junaidatique@gmail.com>
2019-12-11 09:53:26 +0500

    fixing pooost short link with encodeURI

--- packages/shared/helpers/shortLinks.js
+++ packages/shared/helpers/shortLinks.js
@@ -28,7 +28,7 @@ const shortLink = {
     const url = `${process.env.POOST_URL_SHORTNER_LINK}/api/v2/action/shorten?key=`
       + `${process.env.POOOST_URL_SHORTNER_API_KEY} &url=${longURL}&custom_ending=&is_secret=false&response_type=json`;
     // console.log('purl', url);
-    const resp = await fetch(url);
+    const resp = await fetch(encodeURI(url));
     const json = await resp.json();
     // console.log('json', json);
     return json.result;
