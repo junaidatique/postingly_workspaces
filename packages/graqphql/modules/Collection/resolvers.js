2971cd26135b1024aada97383ef0e44e1e97d6a9
Junaid Atique <junaidatique@gmail.com>
2019-12-11 23:44:10 +0500

    changing the collection filter

--- packages/graqphql/modules/Collection/resolvers.js
+++ packages/graqphql/modules/Collection/resolvers.js
@@ -1,14 +1,17 @@
 const CollectionModel = require('shared').CollectionModel;
 const formattedCollection = require('./functions').formattedCollection
 const query = require('shared').query;
-
+const _ = require('lodash');
 
 module.exports = {
   listCollections: async (obj, args, context, info) => {
-    console.log("TCL: args", args)
+    console.log("TCL: listCollections args", args)
     try {
-      let searchQuery = query.createSearchQuery(CollectionModel, args);
-      searchQuery = searchQuery.find({ store: args.filter.storeId })
+      // let searchQuery = query.createSearchQuery(CollectionModel, args);
+      searchQuery = CollectionModel.find({ store: args.filter.storeId });
+      if (!_.isEmpty(args.filter.title)) {
+        searchQuery = searchQuery.where('title', new RegExp(args.filter.title, "i"));
+      }
       const collections = await searchQuery;
       // console.log("TCL: collections", collections)
       return collections.map(collection => {
