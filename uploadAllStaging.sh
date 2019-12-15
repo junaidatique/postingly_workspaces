01765582b0656b68e10b766f9487de9467643cd3
Junaid Atique <junaidatique@gmail.com>
2019-12-15 15:12:35 +0500

    deleting staging lambda pervious versions.

--- uploadAllStaging.sh
+++ uploadAllStaging.sh
@@ -1,12 +1,15 @@
 echo "Uploading";
 cd 'packages/functions'
 pwd
+NODE_ENV=staging sls prune -n 5 --aws-profile postingly-staging -s staging --force
 NODE_ENV=staging sls deploy --aws-profile postingly-staging -s staging --force
 cd ../..
 cd 'packages/rest_api'
 pwd
+NODE_ENV=staging sls prune -n 5 --aws-profile postingly-staging -s staging --force
 NODE_ENV=staging sls deploy --aws-profile postingly-staging -s staging --force
 cd ../..
 cd 'packages/graqphql'
 pwd
+NODE_ENV=staging sls prune -n 5 --aws-profile postingly-staging -s staging --force
 NODE_ENV=staging sls deploy --aws-profile postingly-staging -s staging --force
