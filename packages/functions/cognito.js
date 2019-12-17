7a8834cc11a585ee9ed4ee434b0b9a2ef25590ec
Junaid Atique <junaidatique@gmail.com>
2019-12-17 12:25:32 +0500

    updating conginito functions

--- packages/functions/cognito.js
+++ packages/functions/cognito.js
@@ -3,6 +3,12 @@ const jwt = require("jsonwebtoken");
 
 module.exports = {
   createAuthChallenge: async function (event, context) {
+
+    if (event.source === 'serverless-plugin-warmup') {
+      console.log('WarmUP - Lambda is warm!')
+      await new Promise(r => setTimeout(r, 25));
+      return 'lambda is warm!';
+    }
     if (!event.request.session || event.request.session.length === 0) {
       // For the first challenge ask for a JWT token
       event.response.publicChallengeParameters = {
@@ -18,6 +24,11 @@ module.exports = {
     context.done(null, event)
   },
   defineAuthChallenge: async function (event, context, callback) {
+    if (event.source === 'serverless-plugin-warmup') {
+      console.log('WarmUP - Lambda is warm!')
+      await new Promise(r => setTimeout(r, 25));
+      return 'lambda is warm!';
+    }
     if (!event.request.session || event.request.session.length === 0) {
       // If we don't have a session or it is empty then issue a CUSTOM_CHALLENGE
       event.response.challengeName = "CUSTOM_CHALLENGE";
@@ -35,6 +46,11 @@ module.exports = {
     context.done(null, event)
   },
   verifyAuthChallenge: async function (event, context, callback) {
+    if (event.source === 'serverless-plugin-warmup') {
+      console.log('WarmUP - Lambda is warm!')
+      await new Promise(r => setTimeout(r, 25));
+      return 'lambda is warm!';
+    }
     const jwtSecret = process.env.JWT_SECRET;
     const challengeAnswer = event.request.challengeAnswer;
     if (!jwtSecret || !challengeAnswer) {
