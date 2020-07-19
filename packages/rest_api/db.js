c48283e785bd24a686ad381204ee299059c47196
Junaid Atique <junaidatique@gmail.com>
2020-07-19 19:03:20 +0500

    updating db.

--- packages/rest_api/db.js
+++ packages/rest_api/db.js
@@ -10,8 +10,8 @@ module.exports = {
           conn = await mongoose.connect(process.env.MONGODB_URL, {
             useNewUrlParser: true, useCreateIndex: true, bufferCommands: false,
             bufferMaxEntries: 0,
-            ssl: true,
-            sslCA: returnCerts(),
+            // ssl: true,
+            // sslCA: returnCerts(),
             dbName: process.env.STAGE,
             useUnifiedTopology: true,
             keepAlive: true,
